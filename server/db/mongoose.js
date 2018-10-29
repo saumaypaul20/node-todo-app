23981cf996cb3fbcffe4c5713228ac5209aed6d0
Saumay Paul <saumaypaul20@gmail.com>
2018-10-30 02:26:22 +0530

    mongoose file setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect( process.env.MONGOLAB_URI||'mongodb://localhost:27017/TodoApp');
 
+//connection 
+
 
 module.exports = {
     mongoose
